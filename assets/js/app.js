ed90106065c833141680f3d1d7735e21db613776
Sam <74021826+x39OME@users.noreply.github.com>
2022-11-02 19:31:03 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+
+function menuToggle(){
+  const menuToggle = document.querySelector('.menuToggle');
+  const navigation = document.querySelector('.navigation');
+  
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+}
